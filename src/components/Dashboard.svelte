19cc6a00a8d2cd5279e2f66a0a349e5481af4123
personal <hui-f.gu@hotmail.com>
2023-10-02 20:13:09 +1030

    add dashboard

---
+++ src/components/Dashboard.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import type { Country } from "$lib/appConfig/types";
+  import getDisplayValue from "$lib/getDisplayValue";
+  export let data:Country[];
+  export let width:number;
+  export let height: number;
+  export let year:number;
+  console.log(data)
+
+  $: totalPopulation = data.reduce((total:number, current:Country) => Number(total + current.population), 0 )
+
+  $: GDP = data.map(country => country.income).filter(income => income !== null).map(income => income as number)
+
+  $: lifeExpectation = data.map((country:Country) => country.life_exp).filter(life_exp => life_exp !== null).map(lifeExp => lifeExp as number) 
+
+  $: totalLifeExp = lifeExpectation.reduce((total:number, current) =>  Number(total + current), 0 )
+  $: averageLifeExp = totalLifeExp/lifeExpectation.length
+
+  $: totalGDP = GDP.reduce((total:number, current) =>  Number(total + current), 0 )
+  $: averageGDP = totalGDP/GDP.length
+</script> 
+
+<div class="container">
+  <header>
+    <h1>Dashboard</h1>
+    <div>
+      In year <span class="variable">{year}</span>, we have <span class="variable">{totalPopulation.toFixed(0)}</span> people in <span class="variable">the World</span>, GDP per  capita is $<span class="variable">{averageGDP.toFixed(2)}</span>, and life expectation is around <span class="variable">{averageLifeExp.toFixed(0)}</span> years old.
+    </div>  
+  </header>
+  
+</div>
+
+<style>
+  h1 {
+    color: #00bcd4;
+  }
+  p {
+    color: #00bcd4;
+  }
+  header {
+    text-align: center;
+  }
+
+  .variable {
+    color: #00bcd4;
+    font-size: 20px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,70 @@
 <script lang="ts">
-  export let data;
+  import data from "$lib/data/data.json"
+  import Dashboard from "../components/Dashboard.svelte"
+  let width = 900;
+  let height = 670;
+  let year: number = 1800;
+  $: years = data.map((row: any) => parseInt(row.year));
 
+  $: filteredData = data.filter((row:any) => row.year === year.toString())[0];
+
+  let interval: any;
+  let isRunning: boolean = false;
+  const yearIncrement = () => {
+    if (isRunning === false) {
+      isRunning = true;
+      interval = setInterval(() => {
+        if (year < 2014) {
+          year++;
+        } else {
+          year = 1800;
+          clearInterval(interval);
+          isRunning = false;
+        }
+      }, 100);
+    }
+  };
+  const resetYear = () => {
+    isRunning = false;
+    year = 1800;
+    clearInterval(interval);
+  };
+  const pauseYear = () => {
+    isRunning = false;
+    clearInterval(interval);
+  };
 </script>
+<div class="container" bind:clientHeight={height} >
+  <Dashboard {width} {height} data={filteredData.countries} {year}/>
+</div>
+<div class="control-panel">
+  <button on:click={yearIncrement}>Let time fly</button>
+  <button on:click={pauseYear}>Pause</button>
+  <button on:click={resetYear}>Reset</button>
+  <input
+    class="year-control"
+    type="range"
+    min={Math.min(...years)}
+    max={Math.max(...years)}
+    bind:value={year}
+  />
+</div>
+
+  <style>
+  .year-control {
+    width: 800px;
+  }
+  .control-panel {
+    padding-top:0.5rem;
+    display: flex;
+    justify-content: center;
+  }
+  .container {
+    background-color:white;
+    height:88vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+  }
+
+  </style>
