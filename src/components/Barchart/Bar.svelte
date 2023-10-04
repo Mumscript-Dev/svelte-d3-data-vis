40ee64e087b7f20aef64b9dcef4c396b5abb728f
personal <hui-f.gu@hotmail.com>
2023-10-04 21:04:13 +1030

    with in/out animation and reponsive feature

--- src/components/Barchart/Bar.svelte
+++ src/components/Barchart/Bar.svelte
@@ -11,10 +11,10 @@
   const margin = {
     top: 50,
     bottom: 80,
-    left: 100,
-    right: 80,
+    left: 20,
+    right: 120,
   };
-  $: innerHeight = height - margin.top - margin.bottom - 100;
+  $: innerHeight = height - margin.top - margin.bottom - 50;
   $: innerWidth = width - margin.left - margin.right;
   let filter: string = "Population"
   
@@ -36,24 +36,45 @@
   $: yDomain = filteredData.map(row => row.country) 
   $: yScale = d3.scaleBand().domain(yDomain).range([0, innerHeight]).paddingInner(0.1).paddingOuter(0.2)
   $: xScale = d3.scaleLinear().domain([2000, 1400000000]).range([0, innerWidth])
+  const duration = 150;
+  const barExit = (_node: Element) => {
+    return {
+      duration: duration * 2,
+      css: (t: number) => {
+        return `                                         
+          opacity: ${t};
+        `;
+      },
+    };
+  }
 
+  const barEnter = (_node: Element) => {
+    return {
+      duration: duration * 2,
+      css: (t: number) => {
+        return `
+          opacity: ${t};                                     
+        `;
+      },
+    };
+  }
 </script>
 
-<div class="container">
+<div class="container" bind:clientWidth={width}>
   <header>
     <h3>Top 20 countries in the World by {filter} in Year {year}</h3>
   </header>
-  <svg {width} height={height-100}>
-    <g transform="translate({margin.left}, {margin.top})">
+  <svg {width} height={height-50}>
+    <g transform="translate({margin.left}, {margin.top})" >
       {#each filteredData as row}
-
-      {console.log(yScale(row.country))}
-        <rect x={0} y={yScale(row.country)} height={yScale.bandwidth()} width={xScale(row.population)} fill={colors(row.continent)}  />
-        {#if xScale(row.population) < innerWidth/2}
+      <g in:barEnter out:barExit>
+        <rect x={0} y={yScale(row.country)} height={yScale.bandwidth()} width={xScale(row.population)} fill={colors(row.continent)} />
+        <!-- {#if xScale(row.population) < innerWidth/2} -->
           <text x={xScale(row.population)} dx={5} y={yScale(row.country) +( yScale.bandwidth()/2+5)} text-anchor="start" stroke="white">{row.country}: {getDisplayValue(row.population)}</text>
-        {:else}
+        <!-- {:else}
            <text x={xScale(row.population)/2}  y={yScale(row.country) +( yScale.bandwidth()/2+5)} text-anchor="middle" stroke="white">{row.country}: {getDisplayValue(row.population)}</text>
-        {/if}
+        {/if} -->
+      </g>
       {/each}
     </g>
   </svg>
@@ -63,5 +84,9 @@
   header {
     text-align: center;
     color:#00bcd4;
+  }
+
+  .container {
+    width:80%
   }
 </style>
