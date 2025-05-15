fa020fa17c8a24f97a8f31d942915c907ac8a47f
personal <hui-f.gu@hotmail.com>
2025-05-15 13:31:17 +0930

    test

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,23 @@
+<div class="demo-container">
+	<iframe
+		src="https://notitia-cloud.ap.qlikcloud.com/single/?appid=da8c91fe-88fd-45ef-b573-596621f7ec6f&obj=aBGkzz&theme=horizon&opt=ctxmenu,currsel"
+		style="border: none;width: 560px; height: 460px;"
+		title="Qlik Sense App"
+	/>
+</div>
+
+<style>
+	.demo-container {
+		margin-top: 20px;
+		width: 80%;
+		height: 100%;
+		display: flex;
+		justify-content: end;
+	}
+
+	iframe {
+		width: 80%;
+		height: 80vh;
+		border-radius: 8px;
+	}
+</style>
